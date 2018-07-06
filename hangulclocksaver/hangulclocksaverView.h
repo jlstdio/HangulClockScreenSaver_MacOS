39e982a322ad497e151116b162f8bc2ed84f6225
Joon Lee <robo813@naver.com>
2018-07-06 17:17:05 +0900

    Initial Commit

---
+++ hangulclocksaver/hangulclocksaverView.h
@@ -0,0 +1,13 @@
+//
+//  hangulclocksaverView.h
+//  hangulclocksaver
+//
+//  Created by Joonhee Lee on 06/07/2018.
+//  Copyright © 2018 Joonhee Lee. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface hangulclocksaverView : ScreenSaverView
+
+@end
